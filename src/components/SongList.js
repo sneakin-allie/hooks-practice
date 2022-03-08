96d825313376405244715a4700c5b603e8b338e6
Allison Schulman (she/her) <68669789+sneakin-allie@users.noreply.github.com>
2022-03-08 13:42:05 -0500

    First commit

---
+++ src/components/SongList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import uuid from "react-uuid";
+import NewSongForm from "./NewSongForm";
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'Almost Home', id: 1 },
+    { title: 'Memory Gospel', id: 2 },
+    { title: 'This Wild Darkness', id: 3 }
+  ]);
+  const [age, setAge] = useState(20);
+  const addSong = (title) => {
+    setSongs([...songs, { title, id: uuid() }])
+  }
+  useEffect(() => {
+    console.log('useEffect hook ran', songs)
+  }, [songs])
+  useEffect(() => {
+    console.log('useEffect hook ran', age)
+  }, [age])
+  return ( 
+    <div className="song-list">
+      <ul>
+        {songs.map(song => {
+          return ( <li key={song.id}>{song.title}</li> );
+        })}
+      </ul>
+      <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}>Add 1 to age: {age}</button>
+    </div>
+  );
+}
+ 
+export default SongList;
